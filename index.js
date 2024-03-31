9a1a2f597ac1ced9c59550897e1b30af81cabcea
nunjayi <epenyash@gmail.com>
2024-03-31 13:57:49 +0300

    adding and deleting properties and values from my employee object

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {
+    name:'martin zerkaa',
+    street: '53rd street'
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let updatedEmployee = {...employee}
+    updatedEmployee[key] = value
+    return updatedEmployee
+}
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    let clonedEmployee = {...employee}
+    delete clonedEmployee[key]
+    return clonedEmployee
+
+}
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
